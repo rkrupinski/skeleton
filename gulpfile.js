dad3571b1a234269b1ddae08f003946a736448db
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-01-20 23:04:14 +0100

    whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp')
-	, jshint = require('gulp-jshint');
+  , jshint = require('gulp-jshint');
 
 gulp.task('jshint', function () {
-	gulp.src('./skeleton.js')
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('default'));
+  gulp.src('./skeleton.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('default', function () {
-	gulp.run('jshint');
+  gulp.run('jshint');
 });
