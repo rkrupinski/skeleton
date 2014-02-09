e3b94f22c1d5df5094112c292b8f5198ea49e1ae
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-02-09 01:05:41 +0100

    indentation

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,23 @@ var gulp = require('gulp')
   , karma = require('gulp-karma');
 
 gulp.task('jshint', function () {
-  gulp.src('./skeleton.js')
+  gulp.src('skeleton.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('karma', function () {
-	gulp.src([
-		'skeleton.js',
-		'test/{,*/}*.js'
-	])
-		.pipe(karma({
-			action: 'run',
-			configFile: 'karma.conf.js'
-		}));
+  gulp.src([
+    'skeleton.js',
+    'test/{,*/}*.js'
+  ])
+    .pipe(karma({
+      action: 'run',
+      configFile: 'karma.conf.js'
+    }));
 });
 
-gulp.task('default', ['jshint', 'karma']);
+gulp.task('default', [
+  'jshint',
+  'karma'
+]);
