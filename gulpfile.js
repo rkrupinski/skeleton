d6f44f84485cecc8fe1c36596ea91ea7fd598328
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-02-23 21:50:15 +0100

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp')
   , jshint = require('gulp-jshint')
+  , uglify = require('gulp-uglify')
+  , rename = require("gulp-rename")
   , karma = require('gulp-karma');
 
 gulp.task('jshint', function () {
   gulp.src('skeleton.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function() {
+  gulp.src('skeleton.js')
+    .pipe(uglify())
+    .pipe(rename('skeleton.min.js'))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('karma', function () {
@@ -21,5 +30,6 @@ gulp.task('karma', function () {
 
 gulp.task('default', [
   'jshint',
-  'karma'
+  'karma',
+  'uglify'
 ]);
