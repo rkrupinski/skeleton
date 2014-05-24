47142cd72c0745f8f3c0618309b3ec946e465d91
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-05-24 19:06:34 +0200

    umd -> cjs

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 skeleton
 ========
 Proof of concept jQuery-compatible DOM library skeleton.
-
-Example plugin implementation:
-```js 
-$.fn.funkyText = function (options) {
-  return this.each(function () {
-    this.style.color = options.color || 'pink';
-  });
-};
-```
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,8 @@
 var gulp = require('gulp')
-  , jshint = require('gulp-jshint')
-  , uglify = require('gulp-uglify')
-  , rename = require("gulp-rename");
+  , jshint = require('gulp-jshint');
 
-gulp.task('jshint', function () {
-  gulp.src('skeleton.js')
+gulp.task('lint', function () {
+  gulp.src('./lib/**/*.js')
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
-
-gulp.task('uglify', function() {
-  gulp.src('skeleton.js')
-    .pipe(uglify())
-    .pipe(rename('skeleton.min.js'))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('default', [
-  'jshint',
-  'uglify'
-]);
