24b7faf8e5abaa9690dd583bcb377dd1fb4f3e21
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-07-30 00:22:27 +0200

    Gulpfile tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,7 @@ var gulp = require('gulp')
   , jshint = require('gulp-jshint');
 
 gulp.task('lint', function () {
-  gulp
-      .src([
-        'index.js',
-        './lib/**/*.js'
-      ])
+  return gulp.src('index.js')
       .pipe(jshint('.jshintrc'))
       .pipe(jshint.reporter('jshint-stylish'));
 });
